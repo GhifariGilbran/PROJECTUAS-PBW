70b74b040096f9194ce432a3e854b2b1903344ad
mzamyalfiansyah <m.zamy1933@gmail.com>
2026-06-10 17:38:04 +0700

    nambahin detail sidebar

--- event_detail.php
+++ event_detail.php
@@ -129,10 +129,15 @@ if (isset($_SESSION['toast_msg'])) {
               </a>
             </li>
             <li>
-              <a href="kelola_event.php" class="menu-link active">
+              <a href="kelola_event.php" class="menu-link">
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
                 Kelola Event
               </a>
+            </li>
+            <li>
+              <p class="menu-link" style="color:aqua; margin-left: 20%;">
+                | Detail Event
+              </p>
             </li>
             <li>
               <a href="kelola_pengguna.php" class="menu-link">
@@ -163,6 +168,7 @@ if (isset($_SESSION['toast_msg'])) {
             </li>
           </ul>
         </div>
+
       <?php else: ?>
         <div class="menu-group">
           <span class="menu-title">Menu Utama</span>
--- tambah_event.php
+++ tambah_event.php
@@ -64,6 +64,11 @@ unset($_SESSION['tambah_event_error']);
               </a>
             </li>
             <li>
+              <p href="kelola_event.php" class="menu-link" style="color:aqua; margin-left: 20%;">
+                | Tambah Event
+              </p>
+            </li>
+            <li>
               <a href="kelola_pengguna.php" class="menu-link">
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
                 Kelola Pengguna
